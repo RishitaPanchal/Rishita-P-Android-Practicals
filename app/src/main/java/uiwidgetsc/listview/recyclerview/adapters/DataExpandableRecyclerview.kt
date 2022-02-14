73fb11c99092d847bb68b89f53a62571df891da1
Rishita Panchal <rishitapanchal2707@gmail.com>
2022-02-14 15:03:26 +0530

    Ui widgets C PR3 final commit

---
+++ app/src/main/java/uiwidgetsc/listview/recyclerview/adapters/DataExpandableRecyclerview.kt
@@ -0,0 +1,13 @@
+package uiwidgetsc.listview.recyclerview.adapters
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.Exception
+
+
+class DataExpandableRecyclerview(val moviename: String, val realname: String, val team: String, val appearance: String,
+                                 var imgurl: String, var createdby: String, var publisher: String,
+                                 var bio: String, var expandable: Boolean = false) {
+
+}
